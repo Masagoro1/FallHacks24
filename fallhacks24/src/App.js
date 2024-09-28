577ffada97db5f514b98163c2e9f484f2660def2
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2024-09-28 14:20:45 -0700

    i failed

--- fallhacks24/src/App.js
+++ fallhacks24/src/App.js
@@ -54,20 +54,23 @@ function App() {
 
     const formData = new FormData(event.target);
     const locations = formData.getAll('location');
+    const reduceTime = formData.get('reduceTime') === 'on';
     const res = await fetch('/api/route', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json;charset=UTF-8',
       },
-      body: JSON.stringify({ locations }),
+      body: JSON.stringify({ locations, reduceTime }),
     });
+
     if (!res.ok) {
       const err = await res.text();
       alert(`Something went wrong.\n${err}`);
     } else {
       const data = await res.json();
       setWaypoints(data.waypoints);
+      console.log('Route time:', data.routeTime);
     }
   }
 
