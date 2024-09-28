54acf1c9aac0d77642cfbed8517b5f66dd5e70b1
Arnice123 <91489078+Arnice123@users.noreply.github.com>
2024-09-28 11:32:38 -0700

    fixed the map

--- fallhacks24/server/server.js
+++ fallhacks24/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { SuperfaceClient } = require('@superfaceai/one-sdk');
 const sdk = new SuperfaceClient();
-const PORT = 5000;
+const PORT = 3000;
 app.use(express.json());
 
 async function geocodeLocation(loc) {
